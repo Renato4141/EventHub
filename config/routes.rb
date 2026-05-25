1783023853445f7a28024f7293c4c4d0cf375521
Renato4141 <rfilzauspe@miuandes.cl>
2026-05-24 22:57:13 -0400

    cambio al original

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
-# REEMPLAZAR el archivo completo con esto:
 Rails.application.routes.draw do
   root "pages#home"
 
-  resources :events do
-    member do
-      patch :publish 
-      patch :cancel    
-    end
-    resources :registrations, only: [:create, :destroy] 
-    resources :reviews, only: [:create]                  
-  end
-
+  resources :events
   resources :users, only: [:index, :show]
+
   resources :categories
+  resources :registrations, only: [:index, :show, :create, :destroy]
+  resources :reviews, only: [:index, :show]
   resources :venues
 end

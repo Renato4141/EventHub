fae855db97d18f73dbf5c758276e6cec402e7188
Renato4141 <rfilzauspe@miuandes.cl>
2026-05-24 22:53:43 -0400

    testeo de cambios de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
+# REEMPLAZAR el archivo completo con esto:
 Rails.application.routes.draw do
   root "pages#home"
 
-  resources :events
+  resources :events do
+    member do
+      patch :publish 
+      patch :cancel    
+    end
+    resources :registrations, only: [:create, :destroy] 
+    resources :reviews, only: [:create]                  
+  end
+
   resources :users, only: [:index, :show]
   resources :categories
-  resources :registrations, only: [:index, :show, :create, :destroy]
-  resources :reviews, only: [:index, :show]
   resources :venues
 end
